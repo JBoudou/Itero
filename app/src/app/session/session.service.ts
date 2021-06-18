5ce4e371c333c808faaaa609efb00a1bb63963dc
David <david-gomez@hotmail.es>
2021-06-18 17:08:00 +0200

    changes for email verification

--- app/src/app/session/session.service.ts
+++ app/src/app/session/session.service.ts
@@ -1,16 +1,16 @@
 // Itero - Online iterative vote application
 // Copyright (C) 2020 Joseph Boudou
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -157,7 +157,7 @@ export class SessionService {
           diff = minRefreshTime;
         }
         this._scheduler.schedule(() => { this.refresh(); }, (data.Expires.getTime() - Date.now()) * 0.75);
-        
+
         return true;
       }),
       tap({
@@ -167,6 +167,10 @@ export class SessionService {
         }
       }),
     );
+  }
+
+  verifyEmail(): void {
+    this.session.verifyEmail()
   }
 
   /** Close the current session (if any). */
