b3d71cf7a5efc6b9ddbbc53f9582397bd0916375
Zeng Yifan <l.zeng@outlook.com>
2021-06-18 15:17:56 +0200

    animation version 3 : height depending on the length of contents, content don't disappear suddenly whtn close

--- app/src/app/poll/poll.component.ts
+++ app/src/app/poll/poll.component.ts
@@ -1,5 +1,5 @@
 // Itero - Online iterative vote application
-// Copyright (C) 2020 Joseph Boudou
+// Copyright (C) 2020 Joseph Boudou, Yifan Zeng
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
