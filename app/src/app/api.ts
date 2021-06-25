edfb64ea37e1621ea4de82e10966a66df14035b6
David <david-gomez@hotmail.es>
2021-06-25 20:49:11 +0200

    new class added for the communication between the front end and the middleware

--- app/src/app/api.ts
+++ app/src/app/api.ts
@@ -1,16 +1,16 @@
 // Itero - Online iterative vote application
 // Copyright (C) 2020 Joseph Boudou
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -212,4 +212,9 @@ export class PollNotifAnswerEntry {
 
 export interface ConfirmAnswer {
   Type: string
+}
+
+export class EmailVerification {
+  verified: boolean
+  unverified: boolean
 }
